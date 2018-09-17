14c9c89fd38b091c51107a209a08e67526b20bdf
shimabox <shimabox@gmail.com>
2018-09-17 09:10:31 +0900

    Change base to Bootstrap.

---
+++ tests/functional/web/bootstrap/README.md
@@ -0,0 +1,1 @@
+** This content is based on [Bootstrap](https://getbootstrap.com/)(ver 4.1.3). **
--- tests/functional/web/index.php
+++ tests/functional/web/index.php
@@ -1,16 +1,4 @@
 <?php
 require_once realpath(dirname(__FILE__) . '/.init.php');
 $detect = new Mobile_Detect();
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>test page</title>
-</head>
-<body>
-    <h1 id="welcome">Test</h1>
-    <?php if (! $detect->isMobile()) { ?><p id="pc">PC</p><?php } ?>
-    <?php if ($detect->isMobile()) { ?><p id="sp">SP</p><?php } ?>
-</body>
-</html>
+require_once realpath(dirname(__FILE__) . '/bootstrap/jumbotron.html');
